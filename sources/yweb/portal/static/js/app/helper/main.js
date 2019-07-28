6c6699909acf43cf10ae74f0738efabf8b5bf5b5
yuhongbo <1359320936@qq.com>
2019-07-28 16:56:49 +0800

    个人站点-首页

--- sources/yweb/portal/static/js/app/helper/main.js
+++ sources/yweb/portal/static/js/app/helper/main.js
@@ -61,5 +61,40 @@ const exitFullscreen = () => {
 UTILS.formatDateTime = formatDateTime;
 UTILS.fullScreen = fullScreen;
 UTILS.exitFullscreen = exitFullscreen;
+UTILS.mix = mix;
+
+
+function mix(...mixins) {
+  class Mix {
+    constructor() {
+      for (let mixin of mixins) {
+        copyProperties(this, new mixin()); // 拷贝实例属性
+      }
+    }
+  }
+
+  for (let mixin of mixins) {
+    copyProperties(Mix, mixin); // 拷贝静态属性
+    copyProperties(Mix.prototype, mixin.prototype); // 拷贝原型属性
+  }
+
+  return Mix;
+}
+
+function copyProperties(target, source) {
+  for (let key of Reflect.ownKeys(source)) {
+    if ( key !== 'constructor'
+      && key !== 'prototype'
+      && key !== 'name'
+    ) {
+      let desc = Object.getOwnPropertyDescriptor(source, key);
+      Object.defineProperty(target, key, desc);
+    }
+  }
+}
+// 这样使用
+// class DistributedEdit extends mix(Class1, Class2) {
+//   // ...
+// }
 
 export default UTILS;
